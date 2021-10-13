7932d7e2f781766b0c9867047a2cbc42a92c8bc3
Rémi Palancher <remi-externe.palancher@edf.fr>
2021-10-13 14:57:43 +0200

    tests: install prown with cap_chown in test env
    Otherwise, testing users do have the permission to chown() anything.

--- tests/run.sh
+++ tests/run.sh
@@ -13,5 +13,7 @@ echo compiling
 gcc -o isolate isolate.c
 echo copy into $TMPDIR
 cp -a $(dirname $0)/.. $TMPDIR/
+# set required capability on prown binary
+sudo setcap cap_chown+ep $TMPDIR/src/prown
 echo run isolate
 sudo $TMPDIR/tests/isolate
