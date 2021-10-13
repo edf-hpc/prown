bb368f92ec077464e51c1189d16a57610982aaec
Rémi Palancher <remi-externe.palancher@edf.fr>
2021-10-13 17:32:21 +0200

    tests: add licence/copyright headers

--- tests/isolate.c
+++ tests/isolate.c
@@ -1,3 +1,20 @@
+/*
+ * Prown is a simple tool developed to give users the possibility to
+ * own projects (files and repositories).
+ * Copyright (C) 2021 EDF SA.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
