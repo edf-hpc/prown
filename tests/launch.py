0289a5810d7036c3aaec7fcb215d495c73bb81f5
Rémi Palancher <remi-externe.palancher@edf.fr>
2021-10-13 11:13:04 +0200

    tests: handle user/group management in launch.py
    The testing user/group DB management (with modifications of
    /etc/{passwd,group}) is now handled by launch.py Python script instead
    of isolate.c C program. This removes hard to maintain C code, restrict
    this C code to its initial purpose (isolate tests in specific
    environment).
    
    In the move, the testing users/groups are now declared in defs.yml YAML
    file, close to tests definitions when they were hard-coded in isolate.c
    C program. This makes tests easier to define.

--- tests/launch.py
+++ tests/launch.py
@@ -7,8 +7,42 @@ import hashlib
 import pwd
 import subprocess
 
+TESTS_DEFS_YML='defs.yml'
+
 tmpdir = None
 projects_dir = '/var/tmp/projects'
+
+class User(object):
+
+    def __init__(self, name, uid):
+        self.name = name
+        self.uid = uid
+
+class UsersGroup(object):
+
+    def __init__(self, name, gid):
+        self.name = name
+        self.gid = gid
+        self.users = []
+
+    def add_user(self, name, uid):
+        self.users.append(User(name,uid))
+
+class UsersDB(object):
+
+    def __init__(self, first_uid, first_gid):
+        self.next_uid = first_uid
+        self.next_gid = first_gid
+        self.groups = []
+
+    def add_group(self, group, users):
+
+        new_group = UsersGroup(group, self.next_gid)
+        self.next_gid+=1
+        for user in users:
+            new_group.add_user(user, self.next_uid)
+            self.next_uid+=1
+        self.groups.append(new_group)
 
 class TestDef(object):
 
@@ -21,7 +55,7 @@ class TestDef(object):
         self.stdout = stdout.encode('utf-8')
         self.stderr = stderr.encode('utf-8')
 
-def init_test_env():
+def init_test_env(usersdb):
 
     global tmpdir
     global prown_path
@@ -37,14 +71,41 @@ def init_test_env():
 
     os.mkdir(projects_dir)
 
+    # add user/groups in /etc/{passwd,group}
+    with open('/etc/passwd', 'a') as passwd_fh:
+        with open('/etc/group', 'a') as group_fh:
+            for group in usersdb.groups:
+                line = "%s:x:%u:%s\n" % (group.name, group.gid, ','.join([ user.name for user in group.users ]));
+                group_fh.write(line)
+                for user in group.users:
+                     line = "%s:x:%u:%u::/tmp:/bin/false\n" % (user.name, user.uid, group.gid)
+                     passwd_fh.write(line)
+
+def load_userdb():
+
+    usersdb = None
+    print("load userdb from %s" % (TESTS_DEFS_YML))
+    with open(TESTS_DEFS_YML, 'r', encoding='utf8') as stream:
+        try:
+            usersdb_y = yaml.safe_load(stream)['usersdb']
+            usersdb = UsersDB(usersdb_y['first_uid'], usersdb_y['first_gid'])
+
+            for group, users in usersdb_y['groups'].items():
+                usersdb.add_group(group, users)
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return usersdb
+
 def load_tests_defs():
 
     tests = []
-    print("load tests from defs.yml")
-    with open('defs.yml', 'r', encoding='utf8') as stream:
+    print("load tests from %s" % (TESTS_DEFS_YML))
+    with open(TESTS_DEFS_YML, 'r', encoding='utf8') as stream:
         try:
-            tests_y = yaml.safe_load(stream)
-            for xtest in tests_y['tests']:
+            tests_y = yaml.safe_load(stream)['tests']
+            for xtest in tests_y:
                 tests.append(TestDef(xtest['name'],
                                      xtest['prepare'],
                                      xtest['user'],
@@ -101,6 +162,7 @@ def run_tests(tests):
 
 if __name__ == '__main__':
 
-    init_test_env()
+    usersdb = load_userdb()
+    init_test_env(usersdb)
     tests = load_tests_defs()
     run_tests(tests)
