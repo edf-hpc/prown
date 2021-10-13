66276b27f65e63e3a4ac49d0bdaee94e804b3742
Rémi Palancher <remi-externe.palancher@edf.fr>
2021-10-13 10:27:45 +0200

    tests: better cleanup and preserve perms in run.sh
    Handle cleanup of $TMPDIR in an exit trap for the cleanup to happen
    everytimes, even if the script ends with error before its normal end.
    
    Preserve permissions in $TMPDIR copy so the binary keep its executable
    bit for others.

--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
+
 TMPDIR=$(mktemp -d)
+
+cleanup() {
+  echo removing $TMPDIR
+  sudo rm -rf $TMPDIR
+}
+trap cleanup EXIT
+
 echo compiling
 gcc -o isolate isolate.c
 echo copy into $TMPDIR
-cp -r $(dirname $0)/.. $TMPDIR/
+cp -a $(dirname $0)/.. $TMPDIR/
 echo run isolate
 sudo $TMPDIR/tests/isolate
-echo removing $TMPDIR
-rm -rf $TMPDIR
